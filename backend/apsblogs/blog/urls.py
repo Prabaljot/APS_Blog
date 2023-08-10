355109948be030ded246b6aae9feecc9520f22e1
Prabaljot <jotprabal5@gmail.com>
2023-08-10 19:29:15 +0530

    new app accounts

--- backend/apsblogs/blog/urls.py
+++ backend/apsblogs/blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('blogpost', views.blogpost, name='blogpost'),
-    path('account', views.account, name='account'),
+    path('account', include('account.urls')),
     path('createblog', views.c_blog, name='c_blog'),
 ]
